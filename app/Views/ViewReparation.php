452238cba889fb608c7ef1735e8eb22b35e55bc2
HodeonArtz <martidicianolold@gmail.com>
2024-12-13 17:07:02 +0100

    feat(index): build form to select role

--- app/Views/ViewReparation.php
+++ app/Views/ViewReparation.php
@@ -1,12 +1,28 @@
 <?php 
-  require __DIR__ . '/../vendor/autoload.php';
+
+  // redirect to index.php if user has not submitted index's form
+  if(
+    !isset($_GET["user-role"]) || 
+      
+        !(strtolower($_GET["user-role"]) == "employee" ||
+        strtolower($_GET["user-role"]) == "client")
+      
+    ){
+    header("Location: ../../public/index.php");
+    exit;
+  }
+
+  
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php  ?></title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
@@ -15,15 +31,12 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
     integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous" defer>
   </script>
-
-
-  <title>Car workshop</title>
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Car Workshop</a>
+      <a class="navbar-brand" href="../public/">Car Workshop</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -31,30 +44,15 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="../../public">Home</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
   <main class="container py-4 gap-2">
-    <h1>Welcome!</h1>
-    <form action="../app/Views/ViewReparation.php" method="GET" class=" row row-cols-lg-auto g-3 align-items-center">
-      <div class="col-12">
-        <label for="userRole" class="form-label   ">Enter as: </label>
-
-      </div>
-      <div class="col-12">
-
-        <select name="user-role" id="userRole" class="form-select">
-          <option value="client">Client</option>
-          <option value="employee">Employee</option>
-        </select>
-      </div>
-      <div class="col-12">
-        <button type="submit" class="btn btn-primary">Enter</button>
-      </div>
-    </form>
+    <h1>Dashboard</h1>
+    <form action=""></form>
   </main>
 </body>
 
