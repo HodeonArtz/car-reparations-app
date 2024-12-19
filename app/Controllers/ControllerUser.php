b9abafaccc6297db5ad1e2967ecf86b9e8440f68
HodeonArtz <martidicianolold@gmail.com>
2024-12-19 22:47:41 +0100

    feat(role): implement controller for handling roles

---
+++ app/Controllers/ControllerUser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Services\ServiceUser;
+
+class ControllerUser
+{
+  private ServiceUser $serviceUser;
+
+
+  public function __construct() {
+    $this->serviceUser = new ServiceUser(); 
+  }
+  public function validateRole() : void {
+    if(
+        !$this->serviceUser->getRole() &&
+        // !(isset($_GET["user-role"]) &&
+        $this->serviceUser->validateRole($_GET["user-role"]))
+      {
+
+         header("Location: ../../public/index.php");
+        exit; 
+    }
+  }
+
+  public function getCurrentRole(): string | bool {
+    return $this->serviceUser->getRole();
+  }
+
+  public function getFormattedRole(): string | bool {
+    return ucfirst($this->serviceUser->getRole());
+    
+  }
+
+  public function setRole():void {
+    // if(isset($_GET["user-role"]))
+      $this->serviceUser->setRole($_GET["user-role"]);
+  }
+
+  public function resetRole():void {
+    $this->serviceUser->unsetRole();
+  }
+}
