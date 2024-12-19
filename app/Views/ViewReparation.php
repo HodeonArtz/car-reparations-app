526080adbf42c12268d3429592d69599b3cd02f6
HodeonArtz <martidicianolold@gmail.com>
2024-12-19 22:46:12 +0100

    build(reparation): render reparation

--- app/Views/ViewReparation.php
+++ app/Views/ViewReparation.php
@@ -1,59 +1,42 @@
 <?php 
+  namespace App\Views;
 
-  // redirect to index.php if user has not submitted index's form
-  if(
-    !isset($_GET["user-role"]) || 
-      
-        !(strtolower($_GET["user-role"]) == "employee" ||
-        strtolower($_GET["user-role"]) == "client")
-      
-    ){
-    header("Location: ../../public/index.php");
-    exit;
+use App\Models\Reparation;
+use DateTime;
+
+  class ViewReparation implements ViewBaseInterface
+  { 
+
+    
+    /**
+     * Summary of render
+     * @param mixed $f
+     * @return void
+     */
+    public function render(Reparation $reparation = null): void {
+      if($reparation){
+      ?>
+<div class="card" style="width: 18rem;">
+  <!-- <img src="..." class="card-img-top" alt="..."> -->
+  <div class="card-body">
+    <h5 class="card-title"><?= $reparation->getId() ?></h5>
+
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">
+      <b>Workshop: </b><?= $reparation->getWorkshopName() ?>
+    </li>
+    <li class="list-group-item">
+      <b>Date: </b><?= $reparation->getFormattedRegisterDate() ?>
+    </li>
+    <li class="list-group-item">
+      <b>License Plate: </b><?= $reparation->getLicensePlate() ?>
+    </li>
+  </ul>
+</div>
+<?php 
+      }
+    }
   }
-
   
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php  ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous" defer>
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous" defer>
-  </script>
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="../public/">Car Workshop</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../../public">Home</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main class="container py-4 gap-2">
-    <h1>Dashboard</h1>
-    <form action=""></form>
-  </main>
-</body>
-
-</html>
