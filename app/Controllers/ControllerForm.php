02f032f248b1fd20e578628b752fceb26fe14431
HodeonArtz <martidicianolold@gmail.com>
2024-12-19 22:47:17 +0100

    feat(forms): handle different forms

---
+++ app/Controllers/ControllerForm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class ControllerForm
+{
+  const ACTIONS = [
+    "SELECT_ROLE" => "select_role",
+    "GET_REPARATION" => "get_reparation"
+  ];
+
+  public function handleForm(string $action,callable $functionHandler) : void {
+    // handle when $action is not in self::ACTIONS array.
+    if($_REQUEST["form_action"] !== $action) return;
+    $functionHandler($_REQUEST);
+  }
+}
