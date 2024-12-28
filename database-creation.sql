5cb643e68e3fa24c1fab9dbfd3035e7e15538a3e
HodeonArtz <martidicianolold@gmail.com>
2024-12-28 01:51:32 +0100

    refactor(sql): set date default datetime to CURRENT_TIMESTAMP()

--- database-creation.sql
+++ database-creation.sql
@@ -4,7 +4,7 @@ create table if not exists reparations(
 	id int(4) primary key not null auto_increment,
     uuid char(36) not null,
     workshop_name varchar(12) not null,
-    register_date date not null,
+    register_date date default CURRENT_TIMESTAMP() not null,
     license_plate char(8) not null,
     vehicle_image_filename varchar(255) not null
 );
