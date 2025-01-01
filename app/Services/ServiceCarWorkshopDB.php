73c4a52e78c946cc30b2600f8d39e3a2346781d8
HodeonArtz <martidicianolold@gmail.com>
2025-01-02 00:10:30 +0100

    feat(logs): log database connections and its errors

--- app/Services/ServiceCarWorkshopDB.php
+++ app/Services/ServiceCarWorkshopDB.php
@@ -3,6 +3,9 @@
 namespace App\Services;
 
 use App\Exceptions\DatabaseException;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use mysqli;
 
 class ServiceCarWorkshopDB
@@ -17,6 +20,9 @@ class ServiceCarWorkshopDB
     $this->database_config = parse_ini_file("../../cfg/db_config.ini");
   }
   public function connectDatabase(): mysqli {
+    $log = new Logger("Car Workshop DB");
+    $log->pushHandler(new StreamHandler("../../logs/car-workshop-db.log", Level::Info));
+  
     try {
       $mysqli = new mysqli(
         $this->database_config["host"],
@@ -25,7 +31,9 @@ class ServiceCarWorkshopDB
         $this->database_config["db_name"],
         $this->database_config["port"],
       );
+      $log->info("A connection to the database has been established.");
     } catch (\Throwable $th) {
+      $log->error("An error has ocurred with the database connection: " . $th->getMessage());
       throw new DatabaseException(
         "There has been an error connecting with the database server."
       );
